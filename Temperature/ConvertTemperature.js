28617db7395b555c93cd44533f74d5e0c2a8d7c6
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2022-12-02 21:46:43 +0700

    Create new project

---
+++ Temperature/ConvertTemperature.js
@@ -0,0 +1,31 @@
+﻿document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    var buttonCalc = document.querySelector(".btn-calc");
+
+    buttonCalc.addEventListener("click", function (e) {
+        var celsius = document.getElementById("user-temp-celsius").value;
+
+        if (checkUserValueIsNumber(celsius)) {
+            document.getElementById("temp-fahrenheit-result").value = getFahrenheitFromCelsius(celsius);
+            document.getElementById("temp-kelvin-result").value = getKelvinFromCelsius(celsius);
+        }
+    });
+
+    function getFahrenheitFromCelsius(celsius) {
+        return ((+celsius * 9 / 5) + 32).toFixed(2);
+    }
+
+    function getKelvinFromCelsius(celsius) {
+        return (+celsius + 273).toFixed(2);
+    }
+}
+
+function checkUserValueIsNumber(userValue) {
+    if (userValue === "" || userValue === null || isNaN(userValue)) {
+        alert("Нужно ввести число.");
+        return false;
+    }
+
+    return true;
+}
