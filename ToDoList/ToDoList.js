a604218e5fff1f2e85b1f31eb2685d3330fe02fa
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2022-12-10 23:47:50 +0700

    Made Todo list

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,49 +1,66 @@
-﻿document.addEventListener("DOMContentLoaded", ready);
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var addButton = document.querySelector(".add-button");
+    var newTaskInputText = document.querySelector(".new-task");
+    var bodyTable = document.querySelector(".tasks-list-table tbody");
 
-function ready() {
-    var buttonAdd = document.querySelector(".form-add-tasks input[type=\"button\"]");
-
-    buttonAdd.addEventListener("click", function () {
-        var userTask = document.querySelector(".form-add-tasks input[type=\"text\"]").value;
-
-        console.log(userTask);
-        
-        if (checkUserTask(userTask)){
-            addUserTask(userTask);
-            document.querySelector(".form-add-tasks input[type=\"text\"]").value ="";
+    document.querySelector("form").addEventListener("keydown", function (e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
         }
-       
+    });
 
-    })
+    addButton.addEventListener("click", function () {
+        var newTask = newTaskInputText.value;
 
-    function checkUserTask(userTask) {
-        if (userTask === "" || userTask === null) {
+        if (!checkTaskIsValid(newTask.trim())) {
+            return;
+        }
+
+        var newTaskRow = document.createElement("tr");
+        addNewTask(newTask);
+
+        function addNewTask(newTask) {
+            newTaskRow.innerHTML = "<td class='new-task'></td>" +
+                "<td><button type='button' class='edit-button'>Редактировать</button>" +
+                "<button type = 'button' class='delete-button'>Удалить</button ></td> ";
+
+            newTaskRow.querySelector(".new-task").textContent = newTask;
+
+            newTaskRow.querySelector(".edit-button").addEventListener("click", function () {
+                newTaskRow.innerHTML = "<td class='edit-task'><input type='text>'></td>" +
+                    "<td><button type='button' class='save-button'>Сохранить</button>" +
+                    "<button type = 'button' class='cancel-button'>Отмена</button ></td> ";
+
+                newTaskRow.querySelector(".edit-task input").value = newTask;
+
+                newTaskRow.querySelector(".save-button").addEventListener("click", function () {
+                    var editTask = newTaskRow.querySelector(".edit-task input").value;
+
+                    if (checkTaskIsValid(editTask.trim())) {
+                        addNewTask(editTask);
+                    }
+                });
+
+                newTaskRow.querySelector(".cancel-button").addEventListener("click", function () {
+                    addNewTask(newTask, newTaskRow)
+                });
+            });
+
+            newTaskRow.querySelector(".delete-button").addEventListener("click", function () {
+                newTaskRow.remove();
+            });
+        }
+
+        newTaskInputText.value = "";
+        bodyTable.appendChild(newTaskRow);
+    });
+
+    function checkTaskIsValid(task) {
+        if (task === "" || task === null) {
             alert("Нужно ввести задачу.");
             return false;
         }
 
         return true;
     }
-
-    function addUserTask(userTask) {
-        var tbody = document.querySelector(".table-tasks-list tbody");
-        var newRow = document.createElement("tr");
-        var taskCell = document.createElement("td");
-        var modifyCell = document.createElement("td");
-        taskCell.innerHTML = userTask;
-
-        tbody.appendChild(newRow);
-        newRow.appendChild(taskCell);
-        newRow.appendChild(modifyCell);
-    }
-
-    function isFirstRow() {
-        var tbody = document.querySelector(".table-tasks-list tbody");
-
-
-    }
-
-    function addFirstRow() {
-
-    }
-}
+})
