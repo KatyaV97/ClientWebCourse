129a3dc722a8f32b2160693e8a5c1492bf598c81
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2022-12-15 22:07:03 +0700

    Fixed functions

--- ToDoListJQuery/ToDoListJQuery.js
+++ ToDoListJQuery/ToDoListJQuery.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
+﻿$(function () {
     var addButton = $(".add-button");
     var newTaskInputText = $(".new-task");
     var bodyTable = $(".tasks-list-table tbody");
@@ -12,44 +12,42 @@
     }
 
     $("form").on("keydown", function (e) {
-        if (e.keyCode == 13) {
+        if (e.keyCode === 13) {
             e.preventDefault();
         }
     });
 
     $(".new-task").on("keydown", function (e) {
-        if (e.keyCode == 13) {
+        if (e.keyCode === 13) {
             addTaskHandler();
         }
     });
 
-    addButton.on("click", function () {
+    addButton.click(function () {
         addTaskHandler();
     });
 
     function addTaskHandler() {
-        var newTask = newTaskInputText.val();
+        var newTask = newTaskInputText.val().trim();
         newTaskInputText.val("");
 
-        if (!checkTaskIsValid($.trim(newTask))) {
+        if (!isValidTask(newTask)) {
             alert("Нужно ввести задачу.");
             return;
         }
 
         var newToken = randomToken();
 
-        var newTaskRow = $("<tr></tr>").appendTo(bodyTable)
-                                       .attr("token", newToken);
+        var newTaskRow = $("<tr>").appendTo(bodyTable).attr("token", newToken);
 
         addTask(newTask, newTaskRow);
 
         localStorage.setItem(newToken, newTask);
-
         tokens.push(newToken);
         localStorage.tokens = tokens.join(", ");
     }
 
-    function checkTaskIsValid(task) {
+    function isValidTask(task) {
         if (task === "" || task === null) {
             return false;
         }
@@ -61,19 +59,20 @@
         taskRow.html("<td class='new-task'></td>" +
             "<td><button type='button' class='edit-button'>Редактировать</button>" +
             "<button type = 'button' class='delete-button'>Удалить</button ></td> ")
-            .find(".new-task").text(task);
+            .find(".new-task")
+            .text(task);
 
-        taskRow.find(".edit-button").on("click", function () {
+        taskRow.find(".edit-button").click(function () {
             var editableTask = $(this).closest("tr").find(".new-task").text();
             var editableRow = $(this).closest("tr");
 
             editTask(editableTask, editableRow);
         });
 
-        taskRow.find(".delete-button").on("click", function () {
+        taskRow.find(".delete-button").click(function () {
             $(this).closest("tr").remove();
 
-            var token = taskRow.getAttribute("token")
+            var token = taskRow.attr("token")
             delete localStorage[token];
 
             var index = tokens.indexOf(token);
@@ -89,12 +88,12 @@
             .find(".edit-task input")
             .val(task);
 
-        taskRow.find(".save-button").on("click", function () {
+        taskRow.find(".save-button").click(function () {
             updateTask($(this).closest("tr"));
         });
 
         taskRow.find(".edit-task input").on("keydown", function (e) {
-            if (e.keyCode == 13) {
+            if (e.keyCode === 13) {
                 updateTask($(this).closest("tr"));
             }
         });
@@ -105,19 +104,20 @@
     }
 
     function updateTask(taskRow) {
-        var editTask = taskRow.find(".edit-task input")
-                              .val();
+        var editTask = taskRow.find(".edit-task input").val().trim();
 
-        if (checkTaskIsValid($.trim(editTask))) {
-            addTask(editTask, taskRow);
-            localStorage[taskRow.getAttribute("token")] = editTask;
+        if (!isValidTask(editTask)) {
+            alert("Нужно ввести задачу.");
+            return;
         }
+
+        addTask(editTask, taskRow);
+        localStorage[taskRow.attr("token")] = editTask;
     }
 
     function initTasks() {
         tokens.forEach(token => {
-            var taskRow = $("<tr></tr>").attr("token", token)
-                                        .appendTo(bodyTable);
+            var taskRow = $("<tr>").attr("token", token).appendTo(bodyTable);
 
             var task = localStorage.getItem(token);
 
