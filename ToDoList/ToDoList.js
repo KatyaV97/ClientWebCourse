be82b869e56c1c2ed40e3afd2af76fd484978838
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2022-12-18 20:04:52 +0700

    Simplified storage logic; Fixed mistakes.

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,21 +1,21 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     var addButton = document.querySelector(".add-button");
     var newTaskInputText = document.querySelector(".new-task");
-    var bodyTable = document.querySelector(".tasks-list-table tbody");
+    var tableBody = document.querySelector(".tasks-list-table tbody");
 
-    var storageTokens = localStorage.getItem("tokens");
-    var tokens = [];
+    var tasks = [];
+    var storageTasks = localStorage.getItem("tasks");
 
-    if (storageTokens !== null && storageTokens !== "") {
-        tokens = storageTokens.split(", ");
+    if (storageTasks !== null && storageTasks !== "") {
+        tasks = JSON.parse(storageTasks);
         initTasks();
     }
 
     addButton.addEventListener("click", function () {
-        addTaskHandler();
+        addTask();
     });
 
-    document.querySelector("form").addEventListener("keydown", function (e) {
+    document.querySelector(".form-add-tasks").addEventListener("keydown", function (e) {
         if (e.keyCode == 13) {
             e.preventDefault();
         }
@@ -24,11 +24,11 @@
     //Добавление задачи при нажатии на Enter
     document.querySelector(".new-task").addEventListener("keydown", function (e) {
         if (e.keyCode == 13) {
-            addTaskHandler();
+            addTask();
         }
     });
 
-    function addTaskHandler() {
+    function addTask() {
         var newTask = newTaskInputText.value;
 
         if (!checkTaskIsValid(newTask.trim())) {
@@ -37,17 +37,17 @@
 
         var newTaskRow = document.createElement("tr");
 
-        addTask(newTask, newTaskRow);
+        insertTaskInRow(newTask, newTaskRow);
+
         newTaskInputText.value = "";
-        bodyTable.appendChild(newTaskRow);
+        tableBody.appendChild(newTaskRow);
 
-        var newToken = randomToken();
+        tasks.push({
+            rowIndex: newTaskRow.sectionRowIndex,
+            task: newTask,
+        });
 
-        newTaskRow.setAttribute("token", newToken);
-        localStorage.setItem(newToken, newTask);
-
-        tokens.push(newToken);
-        localStorage.tokens = tokens.join(", ");
+        localStorage.tasks = JSON.stringify(tasks);
     }
 
     function checkTaskIsValid(task) {
@@ -59,10 +59,10 @@
         return true;
     }
 
-    function addTask(task, taskRow) {
+    function insertTaskInRow(task, taskRow) {
         taskRow.innerHTML = "<td class='new-task'></td>" +
             "<td><button type='button' class='edit-button'>Редактировать</button>" +
-            "<button type = 'button' class='delete-button'>Удалить</button ></td> ";
+            "<button type='button' class='delete-button'>Удалить</button ></td> ";
 
         taskRow.querySelector(".new-task").textContent = task;
 
@@ -71,21 +71,17 @@
         });
 
         taskRow.querySelector(".delete-button").addEventListener("click", function () {
+            tasks.splice(taskRow.sectionRowIndex, 1);
+            localStorage.tasks = JSON.stringify(tasks);
+
             taskRow.remove();
-
-            var token = taskRow.getAttribute("token")
-            delete localStorage[token];
-
-            var index = tokens.indexOf(token);
-            tokens.splice(index, 1);
-            localStorage.tokens = tokens.join(", ");
         });
     }
 
     function editTask(task, taskRow) {
-        taskRow.innerHTML = "<td class='edit-task'><input type='text>'></td>" +
+        taskRow.innerHTML = "<td class='edit-task'><input type='text'></td>" +
             "<td><button type='button' class='save-button'>Сохранить</button>" +
-            "<button type = 'button' class='cancel-button'>Отмена</button ></td> ";
+            "<button type='button' class='cancel-button'>Отмена</button ></td> ";
 
         taskRow.querySelector(".edit-task input").value = task;
 
@@ -100,7 +96,7 @@
         });
 
         taskRow.querySelector(".cancel-button").addEventListener("click", function () {
-            addTask(task, taskRow);
+            insertTaskInRow(task, taskRow);
         });
     }
 
@@ -108,25 +104,21 @@
         var editTask = taskRow.querySelector(".edit-task input").value;
 
         if (checkTaskIsValid(editTask.trim())) {
-            addTask(editTask, taskRow);
-            localStorage[taskRow.getAttribute("token")] = editTask;
+            insertTaskInRow(editTask, taskRow);
+
+            tasks[taskRow.sectionRowIndex].task = editTask;
+            localStorage.tasks = JSON.stringify(tasks);
         }
     }
 
     function initTasks() {
-        tokens.forEach(token => {
+        tasks.forEach(taskInfo => {
             var taskRow = document.createElement("tr");
-            var task = localStorage.getItem(token);
+            var task = taskInfo.task;
 
-            taskRow.setAttribute("token", token);
+            insertTaskInRow(task, taskRow);
 
-            addTask(task, taskRow);
-
-            bodyTable.appendChild(taskRow);
-        })
-    }
-
-    function randomToken() {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+            tableBody.appendChild(taskRow);
+        });
     }
 })
