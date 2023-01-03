a761b7f5f223c3d43a59340fd39a35769e925f1f
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2023-01-04 02:36:51 +0700

    fixed mistakes

--- WorkingWithUnderscore/WorkingWithUnderscore.js
+++ WorkingWithUnderscore/WorkingWithUnderscore.js
@@ -43,17 +43,22 @@
     ];
 
     function getPeopleInfoString(people) {
-        return _.chain(people).map(function (person) {
-            return "Name: " + person.name + ", " + "Age: " + person.age;
-        }).join(", \n").value();
+        return _.chain(people)
+            .map(function (person) {
+                return "Name: " + person.name + ", Age: " + person.age;
+            })
+            .join(", \n")
+            .value();
     }
 
     console.log("Список людей:\n" + getPeopleInfoString(people));
 
     function getAverageAge(people) {
-        return _.chain(people).reduce(function (averageAge, person) {
-            return averageAge + person.age;
-        }, 0).value();
+        return _.chain(people)
+            .reduce(function (agePeopleSum, person) {
+                return agePeopleSum + person.age;
+            }, 0)
+            .value() / people.length;
     }
 
     var averageAge = getAverageAge(people);
@@ -61,11 +66,14 @@
     console.log("Средний возраст: " + averageAge);
 
     function getFrom20To30AgeSortPeople(people) {
-        return _.chain(people).filter(function (person) {
-            return person.age >= 20 && person.age <= 30;
-        }).sort(function (person1, person2) {
-            return person1.age - person2.age;
-        }).value();
+        return _.chain(people)
+            .filter(function (person) {
+                return person.age >= 20 && person.age <= 30;
+            })
+            .sort(function (person1, person2) {
+                return person1.age - person2.age;
+            })
+            .value();
     }
 
     var from20To30AgeSortPeople = getFrom20To30AgeSortPeople(people);
@@ -74,37 +82,50 @@
         getPeopleInfoString(from20To30AgeSortPeople));
 
     function getFrom20To30AgeSortByDescWithUniqueNamesPeople(people) {
-        return _.chain(people).filter(function (person) {
-            return person.age >= 20 && person.age <= 30;
-        }).uniq(function (person) {
-            return person.name;
-        }).sort(function (person1, person2) {
-            return person2.age - person1.age;
-        }).value();
+        return _.chain(people)
+            .filter(function (person) {
+                return person.age >= 20 && person.age <= 30;
+            })
+            .uniq(function (person) {
+                return person.name;
+            })
+            .sortBy(function (person) {
+                return person.name;
+            })
+            .map(function (person) {
+                return person.name;
+            })
+            .value();
     }
 
     var from20To30AgeSortByDescWithUniqueNamesPeople = getFrom20To30AgeSortByDescWithUniqueNamesPeople(people);
 
-    console.log("Список людей с уникальными именами возрастом от 20 до 30 лет включительно:\n" +
-        getPeopleInfoString(from20To30AgeSortByDescWithUniqueNamesPeople));
+    console.log("Список уникальных имен возрастом от 20 до 30 лет включительно:\n" +
+        from20To30AgeSortByDescWithUniqueNamesPeople.join(", "));
 
     function getUniquePeopleNamesList(people) {
         var result = {};
 
-        _.chain(people).uniq(function (person) {
-            return person.name;
-        }).each(function (person) {
-            var personName = person.name;
+        _.chain(people)
+            .uniq(function (person) {
+                return person.name;
+            })
+            .each(function (person) {
+                var personName = person.name;
 
-            result[personName] = _.chain(people).filter(function (person) {
-                return person.name === personName;
-            }).size().value();
-        }).value();
+                result[personName] = _.chain(people)
+                    .filter(function (person) {
+                        return person.name === personName;
+                    })
+                    .size()
+                    .value();
+            })
 
         return result;
     }
 
     var uniquePeopleNames = getUniquePeopleNamesList(people);
+    console.log(uniquePeopleNames);
 
     for (var name in uniquePeopleNames) {
         console.log(name + ": " + uniquePeopleNames[name]);
